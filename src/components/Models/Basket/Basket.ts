49be0eccca7ba146561491a2937723038f43b61a
Ekaterina Koloyarskaya <KoloyarskayaEk@yandex.ru>
2026-05-18 14:28:03 +0300

    feat(Basket): emit basket changed event on state update

--- src/components/Models/Basket/Basket.ts
+++ src/components/Models/Basket/Basket.ts
@@ -1,20 +1,31 @@
 import { IProduct } from "../../../types/index.ts";
+import { IEvents } from "../../base/Events.ts";
 
 export class Basket {
   protected catalogBasket: IProduct[] = [];
+  protected events: IEvents;
+
+  constructor(events: IEvents) {
+    this.events = events;
+  }
 
   getCatalog(): IProduct[] {
     return this.catalogBasket;
   }
 
   addProduct(product: IProduct) {
+    if (this.checkProductInBasket(product.id)) {
+      return;
+    }
     this.catalogBasket.push(product);
+    this.events.emit("basket: changed");
   }
 
   removeProduct(id: string) {
     this.catalogBasket = this.catalogBasket.filter((item) => {
       return item.id !== id;
     });
+    this.events.emit("basket: changed");
   }
 
   getAllPrice(): number {
@@ -31,5 +42,6 @@ export class Basket {
 
   clear() {
     this.catalogBasket = [];
+    this.events.emit("basket: changed");
   }
 }
