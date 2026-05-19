32a9161c751fc3aae3f18e4bfa788af5f4a5c792
Ekaterina Koloyarskaya <KoloyarskayaEk@yandex.ru>
2026-05-19 13:24:49 +0300

    feat(catalogProduct): add event emissions for catalog and product state changes

--- src/components/Models/CatalogProduct/CatalogProduct.ts
+++ src/components/Models/CatalogProduct/CatalogProduct.ts
@@ -1,16 +1,21 @@
 import { IProduct } from "../../../types/index.ts";
+import { IEvents } from "../../base/Events.ts";
 
 export class CatalogProduct {
   protected products: IProduct[];
   protected product: IProduct | null;
+  protected events: IEvents;
 
-  constructor() {
+  constructor(events: IEvents) {
+    this.events = events;
     this.products = [];
     this.product = null;
   }
 
   saveCatalog(catalog: IProduct[]) {
     this.products = catalog;
+
+    this.events.emit('catalog: updated', catalog);
   }
 
   getCatalog(): IProduct[] {
@@ -26,6 +31,8 @@ export class CatalogProduct {
 
   saveSelectedProduct(item: IProduct) {
     this.product = item;
+
+    this.events.emit('product: selected', item);
   }
 
   getSelectedProduct(): IProduct | null {
